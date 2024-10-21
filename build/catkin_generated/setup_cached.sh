a93e14a9cff6f7ce77a050a9a023428d274a0f22
felix-lhz <1175906605@qq.com>
2024-10-21 14:38:09 +0800

    rebuild

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/felix/ros-learning/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/usr/local/cuda-11.8/lib64'
+export PATH='/opt/ros/noetic/bin:/usr/local/cuda-11.8/bin:/home/felix/miniconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/snap/bin'
 export PWD='/home/felix/ros-learning/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/felix/ros-learning/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/felix/ros-learning/src:$ROS_PACKAGE_PATH"
