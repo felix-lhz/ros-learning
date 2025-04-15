6903082b909d08c45ba56c126318d086ed878557
felix-lhz <1175906605@qq.com>
2025-04-15 21:57:42 +0800

    delete build

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,10 +5,8 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("ar_track_alvar/ar_track_alvar_msgs")
 subdirs("learning_communication")
 subdirs("robot_vision")
 subdirs("learning_tf")
 subdirs("mrobot_navigation")
-subdirs("ar_track_alvar/ar_track_alvar")
 subdirs("mrobot_description")
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,7 @@
 # new environment variables
 
 # modified environment variables
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/usr/local/cuda-11.8/lib64'
+export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig'
 export PWD='/home/felix/ros-learning/build'
+export PYTHONPATH='/opt/ros/noetic/lib/python3/dist-packages'
